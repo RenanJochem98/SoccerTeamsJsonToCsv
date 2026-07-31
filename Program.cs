5944c226e5bd91cf06952f5256a9c2a07548c1b9
Renan Jochem <renanjochem98@outlook.com>
2026-07-31 08:10:54 -0300

    Add support to pass json input path by args.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,43 @@
 ﻿using SoccerTeamsJsonToCsv;
 
+//...\SoccerTeamsJsonToCsv\bin\Debug\net10.0\ para ...\SoccerTeamsJsonToCsv 
+string? appPath = Directory.GetParent(AppContext.BaseDirectory)?.Parent?.Parent?.Parent?.FullName;
+
+if (string.IsNullOrEmpty(appPath))
+{
+    Console.WriteLine("Não foi possível determinar o caminho da aplicação.");
+    return;
+}
+
 Console.WriteLine("Iniciando processamento...");
 
-string jsonlFilePath = @"C:\Users\renan\Programacao\TesteTecnico\SoccerTeamsJsonToCsv\sample_clubes.jsonl";
-string clubsCsvFilePath = @"C:\Users\renan\Programacao\TesteTecnico\SoccerTeamsJsonToCsv\OutputCsv\clubs.csv";
-string playersCsvFilePath = @"C:\Users\renan\Programacao\TesteTecnico\SoccerTeamsJsonToCsv\OutputCsv\players.csv";
+string jsonlFilePath = Path.Combine(appPath, "sample_clubes.jsonl");
+string clubsCsvFilePath = Path.Combine(appPath, "OutputCsv", "clubs.csv");
+string playersCsvFilePath = Path.Combine(appPath, "OutputCsv", "players.csv");
+
+IEnumerable<string> championships = ["SERIE A", "SERIE B"];
+
+if (args.Length == 0)
+{
+    Console.WriteLine($"Nenhum argumento fornecido. Utilizando valores padrão.: JsonlFilePath: \"{jsonlFilePath}\", Championships: {string.Join(", ", championships)}");
+}
+else
+{
+    jsonlFilePath = args[0].Trim();
+
+    if (args.Length > 1 && !string.IsNullOrEmpty(args[1]))
+    {
+        championships = args[1].Split(',').Select(p => p.Trim());
+    }
+}
 
 try
 {
-    List<string> championships = new() { "SERIE a", "SERIE b" };
+    championships = championships.Select(p => p.ToUpper());
 
-    championships = championships.Select(p => p.ToUpper()).ToList();
-    JsonlToCsvConverterprivate.ProcessJsonStreamAsync(jsonlFilePath, clubsCsvFilePath, playersCsvFilePath, championships).GetAwaiter().GetResult();
+    Console.WriteLine($"Utilizando valores: JsonlFilePath: \"{jsonlFilePath}\", Championships: {string.Join(", ", championships)}");
+
+    JsonlToCsvConverterprivate.ProcessJsonStreamAsync(jsonlFilePath, clubsCsvFilePath, playersCsvFilePath, championships.ToList()).GetAwaiter().GetResult();
 }
 catch (Exception ex)
 {
@@ -20,4 +46,6 @@ catch (Exception ex)
 finally
 {
     Console.WriteLine("Processamento finalizado.");
-}   
+    Console.WriteLine($"O arquivo clubs.csv foi gerado no caminho: {clubsCsvFilePath}");
+    Console.WriteLine($"O arquivo players.csv foi gerado no caminho: {playersCsvFilePath}");
+}
